8763d7c14e1683db7459841a9a7937075ba2642e
zgraja <zgraja@autograf.pl>
2017-05-15 20:35:21 +0200

    Test

--- Scheduler/Scheduler/Controllers/HomeController.cs
+++ Scheduler/Scheduler/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Scheduler.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Aplikacja Gosiaczka";
 
             return View();
         }
