6bbbed49f7cec89b7780e25c6d342e16305b2a93
zgraja <zgraja@autograf.pl>
2017-05-15 21:04:10 +0200

    Models

---
+++ Scheduler/Scheduler/Models/ScheduleItem.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+
+namespace Scheduler.Models
+{
+    public class ScheduleItem
+    {
+        public ScheduleItem()
+        {
+
+        }
+        public string ClassroomName { get; set; }
+        public string Teacher { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
